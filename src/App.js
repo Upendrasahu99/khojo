3920842e87f341fc60625bf59f18ce2d409ed0f3
Upendra Sahu <upendrasahu1199@gmail.com>
2024-08-22 22:44:56 +0530

    Add initial dark theme state and logic
    - Created a state to manage dark theme (true/false)
    
    - Added conditional logic to apply dark theme to the main div
    
    - This commit sets up the foundation for implementing a full dark theme feature in the future

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import Footer from "./components/Footer";
 import Routes from "./components/Routes";
 
 const App = () => {
+
+  const [darkTheme, setDarkTheme] = useState(false);
+console.log(darkTheme);
   return(
-    <div>
+    <div className={darkTheme ? 'dark' : ''}>
+
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 h-screen">
+      </div>
     </div>
   )
 }
