75cefc688add8fa22193908c2f403b7e4f559d33
Upendra Sahu <upendrasahu1199@gmail.com>
2024-08-24 22:41:12 +0530

    Add initial Navbar structure with Tailwind classes

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import Routes from "./components/Routes";
 const App = () => {
 
   const [darkTheme, setDarkTheme] = useState(false);
-console.log(darkTheme);
+
   return(
     <div className={darkTheme ? 'dark' : ''}>
 
       <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 h-screen">
+        <Navbar/>
       </div>
     </div>
   )
