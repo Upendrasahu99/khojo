e304564e1bccc2fa818eb5a0834c97bc1bae5f93
Upendra Sahu <upendrasahu1199@gmail.com>
2024-08-25 23:59:05 +0530

    Initialize React Router and integrate Navbar into routing setup
    - Set up React Router in index.js
    - Wrapped App.js with Router component
    - Integrated Navbar component within the routing structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className='p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200'>
       <div className="flex justify-center items-center space-x-5 w-screen">
-        Navbar
+        <Link to= "/">
+          <p className='text-2xl'>
+            Khojo 
+          </p>
+        </Link>
       </div>
     </div>
   )
