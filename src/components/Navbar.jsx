80ab6d8c94aeff88a75b0a17d0e539e0ae6ee249
Upendra Sahu <upendrasahu1199@gmail.com>
2024-10-16 23:30:38 +0530

    Chnaged icon, title, added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import khojoLogo from '../assets/logo/khojo.png'
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <a className="btn btn-ghost text-xl">daisyUI</a>
+    </div>
+  )
+}
+
+export default Navbar
