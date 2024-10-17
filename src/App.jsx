cfdc67e2741ea49304532a30e203b074443bfd11
Upendra Sahu <upendrasahu1199@gmail.com>
2024-10-17 08:04:20 +0530

    Added Navbar with logo,search-bar and dark theme toggler

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
+  if(darkTheme){
+    document.querySelector('html').setAttribute('data-theme', 'dark');
+  }else{
+    document.querySelector('html').setAttribute('data-theme', 'light');
+  }
   return (
-    <div>App</div>
+    <div>
+      <Navbar setDarkTheme={setDarkTheme} darkTheme={darkTheme}/>
+    </div>
   )
 }
 
